16d3cec6b0a32653af1285715a0405d50b1fde5c
Diana <8112148+chen20xin@user.noreply.gitee.com>
2025-07-16 18:27:37 +0800

    matev2适配

--- harl/envs/multicamera/multicamera_env.py
+++ harl/envs/multicamera/multicamera_env.py
@@ -7,14 +7,16 @@ class MultiCameraEnv:
         self.args = copy.deepcopy(args)
         self.env = mate.make(args["scenario"])
         self.n_agents = self.env.num_cameras
+        #  list[space,space,...]
         self.share_observation_space = self.repeat(self.env.state_space)
         self.observation_space = self.repeat(self.env.observation_space[0])
         self.action_space = self.repeat(self.env.action_space[0])
         self._seed = 0
-        if self.env.action_space[0].__class__.__name__ == "Box":
-            self.discrete = False
-        else:
-            self.discrete = True
+        assert self.env.action_space[0].__class__.__name__ == "Discrete"
+        # if self.env.action_space[0].__class__.__name__ == "Box":
+        #     self.discrete = False
+        # else:
+        #     self.discrete = True
 
         # if "max_cycles" in self.args:
         #     self.max_cycles = self.args["max_cycles"]
@@ -32,17 +34,18 @@ class MultiCameraEnv:
         """
         return local_obs, global_state, rewards, dones, infos, available_actions
         """
-        if self.discrete:
-            obs, rew, done, info = self.env.step(actions.flatten()[0])
-        else:
-            obs, rew, done, info = self.env.step(actions)
+        # if self.discrete:
+        obs, rew, done, info = self.env.step(actions.flatten())
+        # else:
+        #     obs, rew, done, info = self.env.step(actions)
         # self.cur_step += 1
         obs = list(obs)
-        done = self.repeat(done)
-        rew = self.repeat([rew])
+        # done = self.repeat(done)
+        rew = [[item] for item in rew]
         s_obs = self.repeat(self.env.state())
-        for agent in range(self.n_agents):
-            info[agent]["bad_transition"] = True
+        if done[0]:
+            for agent in range(self.n_agents):
+                info[agent]["bad_transition"] = True
         return (
             obs,
             s_obs,
@@ -61,11 +64,11 @@ class MultiCameraEnv:
         return obs, s_obs, self.get_avail_actions()
 
     def get_avail_actions(self):
-        if self.discrete:
-            avail_actions = [[1] * self.action_space[0].n]
-            return avail_actions
-        else:
-            return None
+        # if self.discrete:
+        avail_actions = [[1] * self.action_space[0].n]*self.n_agents
+        return avail_actions
+        # else:
+        #     return None
 
     def render(self):
         self.env.render()
